32f1f0bc5c78f43b9bd1a0b8235def3908a967e7
Pedro Henrique <pedrohenriquesoa@gmail.com>
2022-07-07 14:01:00 -0300

    autotext

---
+++ script.js
@@ -0,0 +1,15 @@
+const text = "One Piece,The series focuses on Monkey D. Luffy, a young man made of rubber, who, inspired by his childhood idol, the powerful pirate Red-Haired Shanks, sets off on a journey from the East Blue Sea to find the mythical treasure";
+
+let index = 0;
+
+function writeText() {
+    document.body.innerText = text.slice(0, index);
+
+    index++;
+
+    if (index > text.length) {
+        index = 0;
+    }
+}
+
+setInterval(writeText, 100);
